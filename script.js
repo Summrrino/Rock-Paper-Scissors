a82418245b23cd7ebeb67e30208872463955f5d0
summrrino <jhaney3@lsu.edu>
2025-02-20 20:41:40 -0600

    added the game logic and rounds to the game to keep score

--- script.js
+++ script.js
@@ -22,3 +22,55 @@ console.log(getHumanChoice());
 let computerScore = 0;
 let humanScore = 0;
 
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        return "It's a tie!";
+    } else if(humanChoice === 'rock' && computerChoice === 'scissors'){
+        humanScore++;
+        return "You win! Rock beats scissors";
+    } else if(humanChoice === 'paper' && computerChoice === 'rock'){
+        humanScore++;
+        return "You win! Paper beats rock";
+    } else if(humanChoice === 'scissors' && computerChoice === 'paper'){
+        humanScore++;
+        return "You win! Scissors beats paper";
+    } else {
+        computerScore++;
+        return "You lose!";
+    }
+
+}
+
+function playGame(){
+
+    let computerScore = 0;
+    let humanScore = 0;
+
+    function playRound(humanChoice, computerChoice){
+        if(humanChoice === computerChoice){
+         return "It's a tie!";
+        } else if(humanChoice === 'rock' && computerChoice === 'scissors'){
+            humanScore++;
+            return "You win! Rock beats scissors";
+        } else if(humanChoice === 'paper' && computerChoice === 'rock'){
+             humanScore++;
+             return "You win! Paper beats rock";
+        } else if(humanChoice === 'scissors' && computerChoice === 'paper'){
+            humanScore++;
+            return "You win! Scissors beats paper";
+        } else {
+            computerScore++;
+            return "You lose!";
+        }
+
+    }
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(humanSelection, computerSelection));
+
+}
+
+for(let i = 0; i < 5; i++){
+    playGame();
+}
