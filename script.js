bbf4ee49a18b229add09e13899b2720b719a1cfb
summrrino <jhaney3@lsu.edu>
2025-02-20 20:27:49 -0600

    added the computers choice and Players Choice

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function getComputerChoice(){
-    
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch(randomNumber){
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
 }
+
+console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let humanChoice = prompt("Enter your choice: rock, paper or scissors");
+    return humanChoice;
+}
+
+console.log(getHumanChoice());
+
+let computerScore = 0;
+let humanScore = 0;
+
